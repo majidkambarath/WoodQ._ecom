a1b43938e950aa7f9253064547a8f9a336b7aacc
ABDUL MAJID K <116737030+majidkambarath@users.noreply.github.com>
2023-01-16 17:38:04 +0530

    finall

--- config/databaseModel.js
+++ config/databaseModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require("dotenv").config();
 
 mongoose.set('strictQuery', true);
-mongoose.connect("mongodb://127.0.0.1:27017/woodQ").then(()=>{
+mongoose.connect("mongodb+srv://abdulmajid:abdulmajid@cluster0.mj7qzoy.mongodb.net/test?retryWrites=true&w=majority").then(()=>{
     console.log("connection successful");
 }).catch((e)=>{
     console.log("no connected");
